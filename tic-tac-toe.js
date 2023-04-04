d14caa0f8b95a86db70cb1173659ef7831b9c3d1
LoganBHolland <109989500+LoganBHolland@users.noreply.github.com>
2023-04-04 18:53:30 -0400

    added ability to use x and o on the board
    you can now place x and o's on the board.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -14,4 +14,49 @@ function setGame() {
                 [' ', ' ', ' '],
                 [' ', ' ', ' ']
             ]
+            for (let r = 0; r < 3; r++) {
+                for (let c = 0; c < 3; c++) {
+                    let tile = document.createElement("div");
+                    tile.id = r.toString() + "-" + c.toString();
+                    tile.classList.add("tile");
+                    if (r == 0 || r == 1) {
+                        tile.classList.add("horizontal-line");
+                    }
+                    if (c == 0 || c == 1) {
+                        tile.classList.add("vertical-line");
+                    }
+                    tile.innerText = "";
+                    tile.addEventListener("click", setTile);
+                    document.getElementById("board").appendChild(tile);
+                }
+            }
+        }
+
+        function setTile() {
+            if (gameOver) {
+                return;
+            }
+        
+            let coords = this.id.split("-");    //ex) "1-2" -> ["1", "2'"]
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+        
+            if (board[r][c] != ' ') { 
+                //already taken spot
+                return;
+            }
+            
+            board[r][c] = currPlayer; //mark the board
+            this.innerText = currPlayer; //mark the board on html
+        
+            //change players
+            if (currPlayer == playerO) {
+                currPlayer = playerX;
+            }
+            else {
+                currPlayer = playerO;
+            }
+        
+            //check winner
+            checkWinner();
         }
